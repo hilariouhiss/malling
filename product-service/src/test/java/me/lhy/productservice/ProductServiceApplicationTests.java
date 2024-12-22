b5090e8c354855216b68b725836e7b2ef41835e9
hilariouhiss <hilariouhiss@outlook.com>
2024-12-22 22:43:12 +0800

    搭建项目框架
    完成父项目、commons模块、nacos和user-service的组合

---
+++ product-service/src/test/java/me/lhy/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lhy.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
